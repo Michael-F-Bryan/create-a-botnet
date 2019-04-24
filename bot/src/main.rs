1ac1d1775a55ddeeb737088a86b3350cadc1ee30
Michael Bryan <michaelfbryan@gmail.com>
2019-04-24 18:15:05 +0800

    Created a rudimentary runtime environment and hello-world plugin

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<Error>> {
+    let binary = env::args().nth(1).ok_or_else(|| {
+        failure::err_msg("Usage: ./bot <wasm-binary>").compat()
+    })?;
+    let wasm = fs::read(&binary)?;
+
+    let imports = wasmer_runtime::imports!();
+    let instance = wasmer_runtime::instantiate(&wasm, &imports)?;
+    instance.call("on_startup", &[])?;
+
+    Ok(())
 }
